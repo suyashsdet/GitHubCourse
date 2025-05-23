f5936670ac77ffa15b06e150b59f019970f17430
Suyash Mudholkar <suyashmudholkar@Suyashs-MacBook-Air.local>
2025-05-23 21:09:56 +0530

    practice commit

--- src/main/java/org/example/suyash1.java
+++ src/main/java/org/example/suyash1.java
@@ -1,12 +1,11 @@
 package org.example;
 
-public class suyash {
-
+public class suyash1 {
     public static void main(String[] args){
         System.out.println("Suyash");
         System.out.println("Suyash");
         System.out.println("Suyash");
         System.out.println("Suyash");
-
+        System.out.println("Suyash");
     }
 }
